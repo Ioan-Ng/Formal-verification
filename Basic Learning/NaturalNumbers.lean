aa7f1753e0d58b6da2768a4398ca854c39db7225
Ioan Ng <isn564@student.bham.ac.uk>
2025-12-24 19:49:01 +0000

    Just looking at how to define types, check and evaluate

---
+++ Basic Learning/NaturalNumbers.lean
@@ -0,0 +1,13 @@
+
+/-We can first define types, so for example m 'is type' of natural numbers defined to be 12-/
+
+def m : Nat := 12
+def b1 : Bool := true
+def b2 : Bool := true
+#check m
+#check b1
+#check b1 && b2
+
+
+#eval 10*8
+#eval b1 || b2
