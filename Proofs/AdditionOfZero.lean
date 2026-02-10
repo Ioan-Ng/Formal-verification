25382cbb10f4cb270987b51fb040eaa6bbb9c22f
Ioan Ng <isn564@student.bham.ac.uk>
2026-02-10 23:30:35 +0000

    Another method of showing n + 0 = n

--- Proofs/AdditionOfZero.lean
+++ Proofs/AdditionOfZero.lean
@@ -5,3 +5,10 @@ theorem add_zero (n : Nat) : 0 + n = n := by
   | succ k ih =>
     rw [Nat.add_succ]
     rw [ih]
+
+theorem add_zero2 (n : Nat) : n + 0  = n := by
+  induction n with
+  | zero =>
+    rfl
+  | succ k ih =>
+    rw [Nat.add_zero]
